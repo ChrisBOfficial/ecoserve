30ef1e42e559f006ce2fac307ffba18dacab3983
vdoan98 <vdoan98@gmail.com>
2019-11-12 14:29:11 -0600

    Add workspace to create new project?

---
+++ src/views/Workspace.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Header/>
+
+    <Footer/>
+  </div>
+</template>
+
+
+<script>
+
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+var request = require('request');
+
+export default {
+  name: 'workspace',
+  components: {
+    Header,
+    Footer
+  }
+}
+
+</script>
