f98e02713287002f84c93d42643af62b1f1bbcf4
Chris Burbach <officialchrisb@icloud.com>
2019-12-01 23:37:08 -0600

    Added PowerShell script to deploy zip on Windows

---
+++ resources/prep-deploy.ps1
@@ -0,0 +1,9 @@
+﻿Set-Location ..\
+Copy-Item .\src\server.js -Destination .\ 
+Copy-Item .\src\assets\projects.json -Destination .\
+Compress-Archive -Path .\dist, .\server.js, .\package.json, .\.env, .\projects.json -DestinationPath .\invasive-species.zip
+
+if($?) {
+    Remove-Item .\server.js
+    Remove-Item .\projects.json
+}
