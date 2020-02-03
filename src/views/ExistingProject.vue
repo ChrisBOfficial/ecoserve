b43ef19626212d2281c21e36c6dd6aa6985421fc
vdoan98 <vdoan98@gmail.com>
2020-02-03 13:55:19 -0600

    Add visualization page

--- src/views/ExistingProject.vue
+++ src/views/ExistingProject.vue
@@ -1,4 +1,112 @@
 <template>
 
+    <div class='existing-project-container'>
+        <Header/>
+        
 
+        <b-container v-show="seen">
+            <b-row>
+                <b-form-select v-model="selected" :select-size="4">
+                    <option v-for="project in projects" :value="project" :key="project">
+                        {{project.name}}
+                    </option>
+                </b-form-select>
+            </b-row>
+            <b-row>
+                <button v-on:click="projectPicked = !projectPicked; seen = !seen">Choose project</button>
+            </b-row>
+        </b-container>
+
+
+
+        <b-container v-show="projectPicked">
+            <b-row class="h-100">
+                <h1 aria-placeholder="CREATE NEW PROJECT"> {{title}} </h1>
+            </b-row>
+            <b-row>
+                <b-form-input v-model="title" placeholder="Enter Project Title"></b-form-input>
+                <b-form-input v-model="description" placeholder="Enter Project Description"></b-form-input>
+            </b-row>
+            <b-row>
+                <h3>Survey: insert survey name</h3>
+                <button> Link to Survey </button>
+            </b-row>
+            <b-row>
+                <h2> Visualization Dashboard </h2>
+                <visualization-dashboard ref="allData"/>
+            </b-row>
+
+
+            <b-row>
+                <b-col>
+                    <b-button v-on:click="createProject" style="background-color:DarkSeaGreen;">SAVE PROJECT</b-button>
+                </b-col>
+                <b-col>
+                    <b-button style="background-color:DarkSeaGreen;" v-b-modal.modal>DELETE PROJECT</b-button>
+                    <b-modal id="modal" title="Warning">
+                        <p class="my-4">Are you sure you want to delete the project?</p>
+                    </b-modal>
+                </b-col>
+                <b-col>
+                    <router-link to="/visualizationpage" tag="button" style="background-color:DarkSeaGreen;">Go To Visualization</router-link>
+                </b-col>
+            </b-row>
+        </b-container>
+
+
+        <Footer/>
+    </div>
 </template>
+
+<script>
+    import {mapActions, mapState} from 'vuex'
+
+	import ProjectForm from '@/components/ProjectForm.vue'
+	import VisualizationDashboard from '@/components/VisualizationDashboard.vue'
+	import Header from '@/components/Header.vue'
+	import Footer from '@/components/Footer.vue'
+    import SurveyBlocks from '@/components/surveyBlocks.vue'
+    
+    export default{
+        components: {
+			ProjectForm,
+			VisualizationDashboard,
+			Header,
+			Footer,
+			SurveyBlocks
+		},
+        data(){
+            return {
+                projectPicked: false,
+                //json: require('../data/projects.json') 
+                //projects: getAllProjects()
+                seen: true
+            }
+        },
+
+        computed: {
+			...mapState({
+				projectNames: state => state.projects.projectNames,
+				projects: state => state.projects.projects
+			})
+        },
+        
+        methods: {
+			...mapActions({
+				//getAllProjects: 'projects/getAllProjects'
+			})
+		}
+        //computed :{
+        //    ...mapGetters([
+        //        'getAllProjects'
+        //    ])
+        //}
+
+        //created(){
+        //    const vm = this;
+        //    vm.getAllProjects();
+        //}
+
+    }
+    
+</script>
---
+++ src/views/VisualizationPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Header/>
+    <div>
+        <b-tabs content-class="mt-2">
+        <b-tab title="Bar Graphs" active><p>I'm the first tab</p></b-tab>
+        <b-tab title="Circular Charts"><p>I'm the second tab</p></b-tab>
+  </b-tabs>
+</div>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+var request = require('request');
+
+export default {
+  name: 'about',
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
