ed301c79edd746b170e728dbb5aae1f4d4b8c14b
Alexis Saltzman <alexis.saltzman@gmail.com>
2020-02-09 21:38:18 -0600

    Create Barchart Component and initial location
    #45 First draft beginnings for bar chart component and test rendering in the visualization tab.

--- src/views/VisualizationPage.vue
+++ src/views/VisualizationPage.vue
@@ -3,7 +3,10 @@
     <Header/>
     <div>
         <b-tabs content-class="mt-2">
-        <b-tab title="Bar Graphs" active><p>I'm the first tab</p></b-tab>
+        <b-tab title="Bar Graphs" active>
+            <p>I'm the first tab</p>
+            <BarChart title="Bar Chart" xKey="name" yKey="amount" conf="confidence" :data="barChartData"/>
+        </b-tab>
         <b-tab title="Circular Charts"><p>I'm the second tab</p></b-tab>
   </b-tabs>
 </div>
@@ -14,14 +17,46 @@
 <script>
 import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
+import BarChart from '@/components/Barchart.vue'
+
 import { mapState } from 'vuex'
 
 export default {
   name: 'about',
   components: {
     Header,
-    Footer
+    Footer,
+    BarChart
   },
+    data: () => ({
+        barChartData: [
+            {
+                name: "Roses",
+                amount: 25,
+                confidence: "low"
+            },
+            {
+                name: "Tulips",
+                amount: 40,
+                confidence: "none"
+            },
+            {
+                name: "Daisies",
+                amount: 15,
+                confidence: "moderate"
+            },
+            {
+                name: "Narcissuses",
+                amount: 9,
+                confidence: "extreme"
+            },
+            {
+                name: "Wallaby",
+                amount: 32,
+                confidence: "high"
+            }
+        ]
+    }),
   computed: {
     ...mapState({
       selectedId: state => state.projects.selectedProjectId
