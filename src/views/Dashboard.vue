3a6a1da95895a2143af7c2c2ad12e0652aefad4f
Alexis Saltzman <alexis.saltzman@gmail.com>
2020-02-10 18:00:09 -0600

    Creating tabs for each species in mock data on barchart tab

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,9 +5,18 @@
         <b-tabs content-class="mt-2">
         <b-tab title="Bar Graphs" active>
             <p>I'm the first tab</p>
-            <BarChart title="Bar Chart" xKey="name" yKey="amount" conf="confidence" :data="barChartData"/>
+            <b-tabs vertical>
+                <b-tab v-for="species in barChartData" :title="species.name">
+                    <p>{{species.name}}</p>
+                    <BarChart title="Bar Chart" xKey="name" yKey="amount" conf="confidence" :data="barChartData"/>
+                </b-tab>
+            </b-tabs>
+
         </b-tab>
-        <b-tab title="Circular Charts"><p>I'm the second tab</p></b-tab>
+        <b-tab title="Circular Charts">
+            <p>I'm the second tab</p>
+            <CirclularChart title="Circle Chart" xKey="service" yKey="mean" :data="CircleChartData"/>
+        </b-tab>
   </b-tabs>
 </div>
     <Footer/>
@@ -18,15 +27,18 @@
 import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
 import BarChart from '@/components/Barchart.vue'
+import CirclularChart from "../components/CirclularChart";
 
 export default {
   name: 'dashboard',
   components: {
+      CirclularChart,
     Header,
     Footer,
     BarChart
   },
     data: () => ({
+        speciesList: ["Roses", "Tulips", "Daisies", "Narcissuses", "Wallaby"],
         barChartData: [
             {
                 name: "Roses",
@@ -53,6 +65,21 @@ export default {
                 amount: 32,
                 confidence: "high"
             }
+        ],
+        CircleChartData: [
+            {
+                service: "Wild Food",
+                mean: -2.5526315789473686
+            },
+            {
+                service: "Forest Production",
+                mean:2.8461538461538462},
+            {service:"Pollination", mean:-2.717948717948718},
+            {service:"Ecological Integrity",mean:-1.4594594594594596},
+            {service:"Biodiversity",mean:-2},
+            {service:"Forage",mean:1.6315789473684212},
+            {service:"Livestock",mean:-1.5135135135135131},
+            {service:"Water",mean:1.4594594594594596}
         ]
     }),
   created: function() {
