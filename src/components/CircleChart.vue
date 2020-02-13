dabc6ff0adb9b0b9c67af17ca19593188af0cf5e
sralph2 <53491281+sralph2@users.noreply.github.com>
2020-02-12 19:54:20 -0600

    most recent version

--- src/components/CircleChart.vue
+++ src/components/CircleChart.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="container" class="svg-container" align="center">
     <svg></svg>
   </div>
 </template>
@@ -14,12 +14,12 @@ export default {
   props: ["values"],
   data: () => {
     return {
-      chart: null
+      circleChart: null
     };
   },
   watch: {
     values: function(val){
-      if (this.chart != null) this.chart.remove();
+      if (this.circleChart != null) this.circleChart.remove();
       this.renderChart(val);
     }
   },
@@ -28,37 +28,65 @@ export default {
   },
   methods: {
     renderChart(issues_val){
-      console.log(issues_val);
       //chart will be drawn here
       const margin = 60;
       const svg_width = 1000;
-      const svg_height = 600;
+      const svg_height = svg_width;
       const chart_width = 1000 - 2 * margin;
       const chart_height = 600 - 2 * margin;
       const innerRadius = 100;
       const outerRadius = 200;
 
-      const svg = d3
-        .select("svg")
+      const svg = d3.select("svg")
         .attr("width", svg_width)
         .attr("height", svg_height);
+    
+
       
       this.circleChart = svg
         .append("g")
         .attr("transform", `translate(${margin}, ${margin})`);
       
-      const xScale = d3
+      console.log("before circle ticks")
+      var i = 0;
+      const numTicks = 11; //needs one more than 10 (20% chunks) to get the 0% line 
+      var sdat = [];
+    
+    for (i=0; i<=numTicks; i++) {
+       sdat[i] = (outerRadius/numTicks) * i;
+    }
+      this.circleChart.selectAll('.circle-ticks').remove();
+
+      var circleAxes = svg
+        .selectAll('.circle-ticks')
+        .data(sdat)
+        .enter().append('svg:g')
+        .attr("class", "circle-ticks");
+
+        // radial tick lines
+        //Note: look into changing zero ticks color to black
+      circleAxes.append("svg:circle")
+        .attr("r", String)
+        .attr("class", "circle")
+        .style("stroke", "#CCC")
+        .style("opacity", 1.00)
+        .style("fill", "none");
+
+      console.log("After circle ticks")
+      console.log(svg_width)
+
+      var x = d3
         .scaleBand()
         .range([0, 2 * Math.PI])    // X axis goes from 0 to 2pi = all around the circle. If I stop at 1Pi, it will be around a half circle
         .align(0)                  // This does nothing
         .domain(issues_val.map(s => s.values)); // The domain of the X axis is the list of impacts.
 
-      const yScale = d3Scale
+      var y = d3Scale
         .scaleRadial()
         .range([innerRadius, outerRadius])   // Domain will be define later.
         .domain([0, 10]); // Domain of Y is from 0 to the max seen in the data, _.maxBy(issues_val, "amount").values
 
-      const ybisScale = d3Scale
+      var ybis = d3Scale
         .scaleRadial()
         .range([innerRadius, 10])   // Domain will be defined later.  _.maxBy(issues_val, "amount").values
         .domain([0, 10]);
@@ -66,7 +94,7 @@ export default {
       this.circleChart = svg.append('g')
         .attr('transform', `translate(${margin}, ${margin})`);
 
-      const upperBars = this.circleChart
+      var upperBars = this.circleChart
         .selectAll("path")
         .data(issues_val)
         .enter()
@@ -88,7 +116,7 @@ export default {
           .padAngle(0.01)
           .padRadius(innerRadius))
       
-      const lowerBars = this.circleChart
+      var lowerBars = this.circleChart
         .selectAll("path")
         .data(issues_val)
         .enter()
@@ -110,8 +138,8 @@ export default {
           .startAngle(function(d) { return x(issues_val); })
           .endAngle(function(d) { return x(issues_val) + x.bandwidth(); })
           .padAngle(0.01)
-          .padRadius(innerRadius))
-
+          .padRadius(innerRadius));
+     
     }
   }
 }
