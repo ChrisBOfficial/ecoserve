c63238856cdfbf04df81245e177dfa5d0da15492
Chris Burbach <officialchrisb@icloud.com>
2020-02-26 20:59:20 -0600

    Updated scripts

--- resources/prep-deploy.ps1
+++
@@ -1,7 +0,0 @@
-﻿Set-Location ..\
-Copy-Item .\src\server.js -Destination .\
-Compress-Archive -Path .\dist, .\server.js, .\package.json, .\.env -DestinationPath .\invasive-species.zip
-
-if($?) {
-    Remove-Item .\server.js
-}
