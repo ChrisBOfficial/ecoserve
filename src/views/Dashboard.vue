cea7a5c8529e4814399047806e8fc5bcb9f4f949
Alexis Saltzman <alexis.saltzman@gmail.com>
2020-04-03 09:24:50 -0500

    added button to export summarized data

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -36,6 +36,13 @@
                 >
                     Download ZIP
                 </b-button>
+                <b-button
+                        v-if="!circularLoading"
+                        @click="downloadJSON"
+                        style="max-width: 20%; background-color: darkseagreen; margin: 1rem 1rem;"
+                >
+                    Download Data
+                </b-button>
             </b-tabs>
         </div>
 
@@ -282,6 +289,37 @@ export default {
                 autoHideDelay: 5000,
                 noCloseButton: true
             });
+        },
+        async downloadJSON() {
+            await this.generateZipFile();
+
+            let exportObj = [];
+            const questions = this.barchartAggregate;
+
+              /*let exportObj = [];
+            const questions = this.survey.questions;
+
+            for (let question in this.survey.questions) {
+                let data = [];
+
+                let subQuestions = questions[question].subQuestions;
+                let questionName = questions[question].questionName;
+                for (let key in subQuestions) {
+                    let dataObj = { subname: subQuestions[key].description, max: 0, min: 0 };
+                    data.push(dataObj);
+                }
+                let questionObj = { questionName: questionName, data: data };
+                exportObj.push(questionObj);*/
+
+
+            let dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(questions, null, 4));
+            let jsonElement = document.createElement("a");
+
+            jsonElement.setAttribute("href", dataStr);
+            jsonElement.setAttribute("download", "summarized.json");
+            document.body.appendChild(jsonElement);
+            jsonElement.click();
+            jsonElement.remove();
         }
     }
 };
