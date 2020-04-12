fbc5900d596007441401f69ac44d299f69f3aab7
Chris Burbach <officialchrisb@icloud.com>
2020-04-11 23:58:57 -0500

    Added collapse to header navigation

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,39 +1,50 @@
 <template>
     <div id="nav">
-        <nav class="navbar navbar-expand-lg navbar-light fixed-top navbar-shrink" id="mainNav">
+        <b-navbar toggleable="sm" type="light" fixed="top" style="background-color: white;">
             <div class="container">
-                <router-link class="navbar-brand" to="/"
-                    ><img
+                <b-navbar-brand to="/">
+                    <img
                         id="homeLink"
                         src="../assets/ecoserve-lower.png"
                         height="100"
                         width="100"
                         title="Home"
                         style="margin:-28px 0px"
-                /></router-link>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item">
-                            <router-link class="nav-link" to="/about">ABOUT</router-link>
-                        </li>
-                        <li v-if="authorized" class="nav-item">
-                            <router-link class="nav-link" to="/projects">PROJECTS</router-link>
-                        </li>
-                        <li v-if="!authorized" class="nav-item">
-                            <a class="nav-link" :href="loginLink">LOGIN</a>
-                        </li>
-                    </ul>
-                    <a v-if="authorized" class="nav-link" href="#" @click="logout">LOGOUT</a>
-                    <router-link v-if="authorized" class="nav-link" to="/auth/settings" style="padding: 2px;"
-                        ><img
-                            src="../assets/settingsGear.png"
-                            alt="Settings"
-                            title="Settings"
-                            style="max-width: 25px; max-height: 25px;"
-                    /></router-link>
-                </div>
+                    />
+                </b-navbar-brand>
+
+                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+                <b-collapse id="nav-collapse" is-nav>
+                    <b-navbar-nav class="ml-auto">
+                        <b-nav-item to="/about" style="margin: auto 2vh;">
+                            ABOUT
+                        </b-nav-item>
+                        <b-nav-item v-if="authorized" to="/projects" style="margin: auto 2vh;">
+                            PROJECTS
+                        </b-nav-item>
+                        <b-nav-item v-if="!authorized" :href="loginLink" style="margin: auto 2vh;">
+                            LOGIN
+                        </b-nav-item>
+                        <b-nav-item v-if="authorized" href="#" @click="logout" style="margin: auto 1vh auto 2vh;">
+                            LOGOUT
+                        </b-nav-item>
+                        <b-nav-item
+                            v-if="authorized"
+                            to="/auth/settings"
+                            style="padding: 2px; margin: auto 0 3px 1.5vh;"
+                        >
+                            <img
+                                src="../assets/settingsGear.png"
+                                alt="Settings"
+                                title="Settings"
+                                style="max-width: 25px; max-height: 25px;"
+                            />
+                        </b-nav-item>
+                    </b-navbar-nav>
+                </b-collapse>
             </div>
-        </nav>
+        </b-navbar>
     </div>
 </template>
 
@@ -60,3 +71,25 @@ export default {
     }
 };
 </script>
+
+<style scoped>
+.navbar-toggler {
+    margin-top: auto 0;
+    max-width: 11%;
+}
+
+@media (max-width: 575px) {
+    .navbar-nav {
+        margin-top: 5vh;
+        border: solid black;
+    }
+}
+
+li.nav-item > a {
+    color: black !important;
+}
+
+li.nav-item :hover {
+    opacity: 0.45;
+}
+</style>
